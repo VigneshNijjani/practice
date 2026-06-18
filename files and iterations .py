600299f3a9b1501d42e748e95c3d20dffd5c20ba
VigneshNijjani <vigneshnijjani@gmail.com>
2026-06-18 17:14:45 +0530

    question

---
+++ files and iterations .py
@@ -0,0 +1,9 @@
+# Objective: Analyze a text file efficiently.
+# Input: Text file
+# Expected Output:
+# ● Line count
+# ● Word count
+# Mandatory Requirements:
+# ● Use generator for file reading
+# ● Use list comprehension
+# ● Use decorator to log execution time
