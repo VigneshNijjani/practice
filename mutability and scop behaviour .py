bc333bdee90a0699ed835ccee7767186d6ba3de9
VigneshNijjani <vigneshnijjani@gmail.com>
2026-06-19 13:55:54 +0530

    question

---
+++ mutability and scop behaviour .py
@@ -0,0 +1,9 @@
+# Objective: Demonstrate mutability and scope behavior.
+
+# Input: 
+# List and tuple
+
+# Expected Output:
+# ● List modification reflected
+# ● Tuple unchanged
+# ● Slicing results
